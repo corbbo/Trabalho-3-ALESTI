05fd5956ba868ad33cd65aa22e15631603bfdf08
Luigi <lumikich@gmail.com>
2023-05-30 09:08:10 -0300

    updated material de apoio

---
+++ Master/CharNode.cpp
@@ -0,0 +1,8 @@
+#include "CharNode.hpp"
+
+CharNode::CharNode(char element, string significado) {}
+CharNode CharNode::addChild(char element, string significado) {}
+int CharNode::getSubtreesSize() {}
+CharNode *CharNode::getSubtree(int idx) {}
+string CharNode::getWord() {}
+CharNode CharNode::*findChildChar(char element) {}
---
+++ Master/Palavra.cpp
@@ -0,0 +1,12 @@
+#include "Palavra.hpp"
+
+// construtor
+Palavra::Palavra(string palavra, string significado)
+{
+    this->palavra = palavra;
+    this->significado = significado;
+}
+
+string Palavra::getPalavra() { return palavra; }
+
+string Palavra::getSignificado() { return significado; }
